4a1a3a7e6e2c969ba4ef1ad3df64a384bc1c79aa
chinawin47 <chinawin47@gmail.com>
2024-03-23 22:15:10 +0700

    Finish UI

---
+++ Assets/Script/AnimationCredit.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AnimationCredit : MonoBehaviour
+{
+ 
+    public void LoadSceneOnAnimationEnd()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
